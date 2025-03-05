519d60c301dff8125ad960cf73b366b8348b30ea
mauyz <tsiorymauyz@gmail.com>
2025-03-05 12:31:02 +0300

    fix unresolved imports

--- src/main/java/org/sid/ebankingbackend/entities/CurrentAccount.java
+++ src/main/java/org/sid/ebankingbackend/entities/CurrentAccount.java
@@ -1,16 +1,18 @@
 package org.sid.ebankingbackend.entities;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("CA")
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 public class CurrentAccount extends BankAccount {
     private double overDraft;
 }
--- src/main/java/org/sid/ebankingbackend/entities/SavingAccount.java
+++ src/main/java/org/sid/ebankingbackend/entities/SavingAccount.java
@@ -1,15 +1,17 @@
 package org.sid.ebankingbackend.entities;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("SA")
 @Data @NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 public class SavingAccount extends BankAccount {
     private double interestRate;
 }
