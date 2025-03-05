a8edcfabcc286718c344f8703075ea31ef0c58a4
mauyz <tsiorymauyz@gmail.com>
2025-03-05 12:27:55 +0300

    fix imports

--- src/main/java/org/sid/ebankingbackend/entities/AccountOperation.java
+++ src/main/java/org/sid/ebankingbackend/entities/AccountOperation.java
@@ -3,12 +3,12 @@ package org.sid.ebankingbackend.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.sid.ebankingbackend.enums.OperationType;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+
 import java.util.Date;
-@Entity
+@Entity 
 @Data @NoArgsConstructor @AllArgsConstructor
 public class AccountOperation {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
