90a6d6a80fa1229b23d8100ff0596d62351138c4
mauyz <tsiorymauyz@gmail.com>
2025-03-05 12:29:00 +0300

    fix unresolved import

--- src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
+++ src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.sid.ebankingbackend.enums.AccountStatus;
-
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Date;
 import java.util.List;
+
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "TYPE",length = 4)
