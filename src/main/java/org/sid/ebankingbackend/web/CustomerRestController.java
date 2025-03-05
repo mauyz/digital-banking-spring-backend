2db6436f487714104ad25ebecba4af4b5fb37ebd
mauyz <tsiorymauyz@gmail.com>
2025-03-05 15:22:20 +0300

    remove unnecessary path names

--- src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
@@ -20,7 +20,7 @@ public class CustomerRestController {
         return bankAccountService.listCustomers();
     }
     @GetMapping("/customers/search")
-    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
+    public List<CustomerDTO> searchCustomers(@RequestParam(defaultValue = "") String keyword){
         return bankAccountService.searchCustomers("%"+keyword+"%");
     }
     @GetMapping("/customers/{id}")
