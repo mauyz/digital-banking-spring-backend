a8db105e4242e193e08a3ba3bc2be12f31e99ff0
mauyz <tsiorymauyz@gmail.com>
2025-03-05 12:30:01 +0300

    fix unused import

--- src/main/java/org/sid/ebankingbackend/dtos/CurrentBankAccountDTO.java
+++ src/main/java/org/sid/ebankingbackend/dtos/CurrentBankAccountDTO.java
@@ -1,11 +1,14 @@
 package org.sid.ebankingbackend.dtos;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import org.sid.ebankingbackend.enums.AccountStatus;
 
 import java.util.Date;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class CurrentBankAccountDTO extends BankAccountDTO {
     private String id;
     private double balance;
