87c69c3633e6a59e6751b5702a17d54e35e75089
mauyz <tsiorymauyz@gmail.com>
2025-03-06 14:53:18 +0300

    implement SecurityController

--- src/main/java/org/sid/ebankingbackend/web/SecurityController.java
+++ src/main/java/org/sid/ebankingbackend/web/SecurityController.java
@@ -1,15 +1,59 @@
 package org.sid.ebankingbackend.web;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@AllArgsConstructor
 public class SecurityController {
 
-	@GetMapping("/profile")
+	private AuthenticationManager authenticationManager;
+
+	private JwtEncoder jwtEncoder;
+
+	@GetMapping("/auth/profile")
 	public Authentication getProfile(Authentication authentication) {
 		return authentication;
+	}
+
+	@PostMapping("/auth/login")
+	public Map<String, String> login(String username, String password) {
+
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+		Instant instant = Instant.now();
+
+		String scope = authentication.getAuthorities().stream().map(role -> role.getAuthority())
+				.collect(Collectors.joining(" "));
+
+		JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder().issuedAt(instant)
+				.expiresAt(instant.plus(1, ChronoUnit.HOURS)).subject(username).claim("scope", scope).build();
+
+		JwtEncoderParameters parameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS512).build(),
+				jwtClaimsSet);
+
+		String jwt = jwtEncoder.encode(parameters).getTokenValue();
+
+		return Map.of("accessToke", jwt);
+
 	}
 
 }
