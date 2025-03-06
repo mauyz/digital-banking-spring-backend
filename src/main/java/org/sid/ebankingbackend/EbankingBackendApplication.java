d17fdaf0301524afc8a4dccd4a26f878bbf74caa
mauyz <tsiorymauyz@gmail.com>
2025-03-06 17:42:10 +0300

    remove unused imports

--- src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
@@ -13,9 +13,6 @@ import org.sid.ebankingbackend.services.BankAccountService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class EbankingBackendApplication {
