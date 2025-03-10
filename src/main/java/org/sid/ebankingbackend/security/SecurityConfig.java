b5d6b4177af48b24d11ea4ef58672eafca44622a
mauyz <tsiorymauyz@gmail.com>
2025-03-10 11:34:59 +0300

    add JwtAuthenticationConverter

--- src/main/java/org/sid/ebankingbackend/security/SecurityConfig.java
+++ src/main/java/org/sid/ebankingbackend/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package org.sid.ebankingbackend.security;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,10 +20,11 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
@@ -34,16 +37,18 @@ public class SecurityConfig {
 	private String secretKey;
 
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UrlBasedCorsConfigurationSource source)
+			throws Exception {
 
 		httpSecurity.formLogin(customizer -> customizer.disable());
 		httpSecurity.sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		httpSecurity.csrf(crsf -> crsf.disable());
-		httpSecurity.cors(Customizer.withDefaults());
+		httpSecurity.cors(cors -> cors.configurationSource(source));
 		httpSecurity.authorizeHttpRequests(customizer -> customizer
 				.requestMatchers("/auth/login/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll());
 		httpSecurity.authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated());
-		httpSecurity.oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()));
+		httpSecurity.oauth2ResourceServer(
+				oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
 		return httpSecurity.build();
 	}
@@ -58,27 +63,36 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	JwtEncoder jwtEncoder() {
-
+	JwtEncoder jwtEncoder() {	
 		return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
 	}
 
 	@Bean
 	JwtDecoder jwtDecoder() {
-		SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "RSA");
-
+		SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA512");	
 		return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
 	}
-	
+
 	@Bean
-	 CorsConfigurationSource configurationSource() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.addAllowedOrigin("*");
-		corsConfiguration.addAllowedMethod("*");
-		corsConfiguration.addAllowedHeader("*");
-		
+	JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+		grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+		return jwtAuthenticationConverter;
+	}
+
+	@Bean
+	UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**",corsConfiguration);
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 }
