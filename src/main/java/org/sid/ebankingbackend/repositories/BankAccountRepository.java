f0ab44b16a37fe6ec61b25ef9d22478cf9ee91b2
mauyz <tsiorymauyz@gmail.com>
2025-03-10 15:53:14 +0300

    add findByCustomerId method

--- src/main/java/org/sid/ebankingbackend/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/ebankingbackend/repositories/BankAccountRepository.java
@@ -1,7 +1,11 @@
 package org.sid.ebankingbackend.repositories;
 
+import java.util.List;
+
 import org.sid.ebankingbackend.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+	List<BankAccount> findByCustomerId(Long customerId);	
 }
+		
